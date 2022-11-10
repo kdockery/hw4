101f74cdd7ff9cb7944bf3fdf0ae0d8498329e61
Kyle Dockery <kdockery@usc.edu>
2022-11-10 23:29:31 +0000

    adding equal paths files

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -3,11 +3,25 @@ using namespace std;
 
 
 // You may add any prototypes of helper functions here
-
+bool checkLength(Node * root, int *x, int length);
 
 bool equalPaths(Node * root)
 {
     // Add your code below
+    //
+    int x=0;
+    int length=0;
+    return checkLength(root, &x, length);
+}
 
+bool checkLength(Node * root, int *x, int length){
+  if (root->left == NULL && root->right == NULL){
+    if (*x == 0){
+      *x = length;
+    }
+    return (*x == length);
+  }
+  return checkLength(root->left, x, length+1);
+  return checkLength(root->right, x, length+1);
 }
 
